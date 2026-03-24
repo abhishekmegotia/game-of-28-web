c649c126b1967922c68c0d2e84ec18ba05bc5101
abhishek <abhishekmegotia@gmail.com>
2026-03-23 22:50:19 -0700

    Fix: use RelayServerData with IsWebSocket flag, fullscreen canvas, no profiler

--- Build/WebBuild.loader.js
+++ Build/WebBuild.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 76200594,
+    wasmFileSize: 76200723,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
